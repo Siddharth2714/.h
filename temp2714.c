a30ab3f77cc9f45f789fb1b7a18542b6ed531b7e
Siddharth2714 <92843916+Siddharth2714@users.noreply.github.com>
2021-11-03 12:13:25 +0530

    Add files via upload

---
+++ temp2714.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int celsius;
+	float fahrenheit;
+     celsius = 10;
+	fahrenheit = celsius*1.8+32;
+	printf("total temperature in fahrenheit is %f",fahrenheit);
+	
+}
